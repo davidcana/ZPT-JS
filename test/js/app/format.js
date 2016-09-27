fcf229cd574e33ae927a8a054d14c4295cbf3c8e
David Cana Lopez <david.javapagetemplates@gmail.com>
2016-09-27 16:11:15 +0200

    Added tests about formatting

---
+++ test/js/app/format.js
@@ -0,0 +1,18 @@
+QUnit.test( "Simple format test", function( assert ) {
+    assert.equal( $('#t1-1').html() , "test" );
+    assert.equal( $('#t1-2').html() , "TEST" );
+    assert.equal( $('#t1-3').html() , "variable test" );
+    assert.equal( $('#t1-4').html() , "VARIABLE TEST" );
+});
+
+QUnit.test( "Format with arguments test", function( assert ) {
+    assert.equal( $('#t2-1').html() , "1.38" );
+    assert.equal( $('#t2-2').html() , "1.6" );
+    assert.equal( $('#t2-3').html() , "0.33" );
+});
+
+QUnit.test( "Custom formatters test", function( assert ) {
+    assert.equal( $('#t3-1').html() , "$150" );
+    assert.equal( $('#t3-2').html() , "$225" );
+    assert.equal( $('#t3-3').html() , "$1225" );
+});
