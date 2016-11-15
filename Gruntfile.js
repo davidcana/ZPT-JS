aa441c417f05620265b28f6c0284e34e57c9855c
David Cana Lopez <david.javapagetemplates@gmail.com>
2016-11-15 13:23:47 +0100

    Added I18n and i18nHelper exports to js/app/main.js; js/app/main.js set as main in package.json; userguide.html updated; Gruntfile.js updated, removed useless configurations

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,26 +2,6 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON( 'package.json' ),
-        concat: {
-            options: {
-                separator: '\n'
-            },
-            dist: {
-                src: [ 'js/app/context.js', 
-                      'js/app/*.js' ],
-                dest: 'dist/<%= pkg.name %>-<%= pkg.version %>.js'
-            }
-        },
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            dist: {
-                files: {
-                    'dist/<%= pkg.name %>.min.js': [ '<%= concat.dist.dest %>' ]
-                }
-            }
-        },
         qunit: {
             files: [ 'test/**/*.html', '!test/index.html', '!test/externalMacros-definitions.html' ]
         },
@@ -228,11 +208,8 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.loadNpmTasks('grunt-browserify');
     
--- js/app/main.js
+++ js/app/main.js
@@ -7,3 +7,7 @@ exports.run = function( options ){
     var parser =  new Parser( options );
     parser.run();
 };
+
+/* I18n and i18nHelp classes */
+exports.I18n = require( './i18n.js' );
+exports.i18nHelper = require( './i18nHelper.js' );
