34509cce7916eba016a6b643a48e52773b56d157
David Cana Lopez <david.javapagetemplates@gmail.com>
2016-11-16 12:36:49 +0100

    Added support RequireJS module pattern

--- js/app/main.js
+++ js/app/main.js
@@ -11,3 +11,10 @@ exports.run = function( options ){
 /* I18n and i18nHelp classes */
 exports.I18n = require( './i18n.js' );
 exports.i18nHelper = require( './i18nHelper.js' );
+
+/* Support RequireJS module pattern */
+if ( typeof define === 'function' && define.amd ) {
+    define( 'zpt.run', exports.run );
+    define( 'zpt.I18n', exports.I18n );
+    define( 'zpt.i18nHelper', exports.i18nHelper );
+}
