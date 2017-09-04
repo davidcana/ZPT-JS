8e478a639ec4eac7c9f76ba2558d0377ff43b050
David Cana <david.javapagetemplates@gmail.com>
2017-09-04 14:46:20 +0200

    Some simple refactoring done at metalUseMacro.js.

--- js/app/attributes/METAL/metalUseMacro.js
+++ js/app/attributes/METAL/metalUseMacro.js
@@ -16,27 +16,10 @@ var METALUseMacro = function( macroExpressionToApply, defineToApply ) {
     
     var process = function( scope, node ){
 
+        // Init some vars
         var macroKey = scope.getResolver().getMacroKey( macroExpression, scope );
-        
         var tags = context.getTags();
         var newNode = scope.getResolver().getNode( macroKey, scope ); 
-            
-        // Build define tag
-        var fullDefine = undefined;
-        var macroData = scope.getResolver().getMacroData( macroKey );
-        if ( macroData.url ){
-            var externalMacroUrlDefine = context.getConf().externalMacroUrlVarName + context.getConf().expressionDelimiter + "'" + macroData.url + "'";
-            fullDefine = define? 
-                         define + context.getConf().defineDelimiter + context.getConf().expressionDelimiter + externalMacroUrlDefine: 
-                         externalMacroUrlDefine;
-        } else {
-            fullDefine = define;
-        }
-        
-        // Copy talDefine attribute from use-macro tag to the macro tag
-        if ( fullDefine ) {
-            newNode.setAttribute( tags.talDefine, fullDefine );
-        }
         
         // Hide use macro node
         node.style.display = 'none';
@@ -44,10 +27,43 @@ var METALUseMacro = function( macroExpressionToApply, defineToApply ) {
         // Remove style attribute to force showing the new node
         newNode.removeAttribute( 'style' );
 
+        // Set tal define
+        updateTalDefineAttribute( scope, macroKey, tags, newNode );
+        
         // Fill slots
+        fillSlots( scope, node, tags, newNode );
+
+        // Add the macro node
+        node.parentNode.insertBefore( newNode, node.nextSibling );
+    };
+
+    var updateTalDefineAttribute = function( scope, macroKey, tags, newNode ){
+        
+        // Build define tag
+        var fullDefine = undefined;
+        var macroData = scope.getResolver().getMacroData( macroKey );
+        if ( macroData.url ){
+            var externalMacroUrlDefine = context.getConf().externalMacroUrlVarName + context.getConf().expressionDelimiter + "'" + macroData.url + "'";
+            fullDefine = define? 
+                define + context.getConf().defineDelimiter + context.getConf().expressionDelimiter + externalMacroUrlDefine: 
+            externalMacroUrlDefine;
+        } else {
+            fullDefine = define;
+        }
+
+        // Copy talDefine attribute from use-macro tag to the macro tag
+        if ( fullDefine ) {
+            newNode.setAttribute( tags.talDefine, fullDefine );
+        }
+    };
+    
+    var fillSlots = function( scope, node, tags, newNode ){
+        
         $( node ).find( "[" + tags.metalFillSlot + "]" ).each(
+            
             function( index, value ) {
-                var slotIdExpressionString = $(this).attr( tags.metalFillSlot );
+                
+                var slotIdExpressionString = $( this ).attr( tags.metalFillSlot );
                 var slotIdExpression = expressionBuilder.build( slotIdExpressionString );
                 var slotId = slotIdExpression.evaluate( scope );
                 //console.log( 'replacing ' + slotId + '...' );
@@ -56,26 +72,23 @@ var METALUseMacro = function( macroExpressionToApply, defineToApply ) {
                 if ( ! slotId ){
                     return;
                 }
-                
+
                 var slotContent = $( this )[0].cloneNode( true );
                 var currentNode = $( newNode ).find(
-                        "[" + tags.metalDefineSlot + "='" + slotId + "']")[0];
+                    "[" + tags.metalDefineSlot + "='" + slotId + "']")[0];
                 if ( ! currentNode ){
                     throw 'Slot "' + slotId + '" in expression "' + slotIdExpressionString +'" not found!';
                 }
                 currentNode.parentNode.insertBefore( 
-                        slotContent, 
-                        currentNode.nextSibling );
+                    slotContent, 
+                    currentNode.nextSibling );
 
                 slotContent.removeAttribute( tags.metalFillSlot );
                 currentNode.remove();
             }
         );
-
-        // Add the macro node
-        node.parentNode.insertBefore( newNode, node.nextSibling );
     };
-
+    
     return {
         process: process
     };
