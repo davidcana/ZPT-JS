8894f89d7820b98fb400fd851429a8236a7dc6ba
David Cana Lopez <david.javapagetemplates@gmail.com>
2019-04-30 20:59:09 +0200

    ZPT version of web updated. Some doc pages added.

--- docs/js/zpt.js
+++ docs/js/zpt.js
@@ -1,3 +1,23 @@
+// This is needed to make the git pages work
+zpt.context.getConf().externalMacroPrefixURL = location.pathname.startsWith( '/Zcrud' )? '/Zcrud/': '/';
+
+// Invoke ZPT
+zpt.run(
+    {
+        command: 'preload',
+        root: [ 
+            document.getElementById( 'commonHeader' ), 
+            document.getElementById( 'commonFooter' )
+        ],
+        dictionary: {},
+        declaredRemotePageUrls: [ 'templates.html' ],
+        callback: function(){
+            zpt.run();
+        }
+    }
+);
+
+/*
 // Invoke ZPT
 var zptParser = zpt.buildParser({
     root: [ 
@@ -16,3 +36,4 @@ zptParser.init(
         zptParser.run();
     }
 );
+*/
