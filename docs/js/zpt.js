48fb6cfd49a45dc396e898fcda91b91b93d1d37a
David Cana Lopez <david.javapagetemplates@gmail.com>
2019-05-01 12:57:38 +0200

    Background changed. Some docs files updated. Some docs files added: i18n.html and macros.html.

--- docs/js/zpt.js
+++ docs/js/zpt.js
@@ -16,24 +16,3 @@ zpt.run(
         }
     }
 );
-
-/*
-// Invoke ZPT
-var zptParser = zpt.buildParser({
-    root: [ 
-        document.getElementById( 'commonHeader' ), 
-        document.getElementById( 'commonFooter' )
-    ],
-    dictionary: {},
-    declaredRemotePageUrls: [ 'templates.html' ]
-});
-
-// This is needed to make the git pages work
-zpt.context.getConf().externalMacroPrefixURL = location.pathname.startsWith( '/Zcrud' )? '/Zcrud/': '/';
-
-zptParser.init(
-    function(){
-        zptParser.run();
-    }
-);
-*/
