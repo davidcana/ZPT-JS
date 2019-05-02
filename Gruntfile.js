951662a8bb7e258cc95ae7d3256979aa22b25cfe
David Cana Lopez <david.javapagetemplates@gmail.com>
2019-05-02 19:33:18 +0200

    grunt-contrib-copy added to package.json and to Gruntfile.js. docs/lib/zpt.min.js file updated.

--- Gruntfile.js
+++ Gruntfile.js
@@ -353,6 +353,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-browserify');
     
