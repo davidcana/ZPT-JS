53c1a5226ff7a84364bd4e14bfbd91973fd3f4a8
David Cana <david.javapagetemplates@gmail.com>
2019-05-23 13:47:02 +0200

    Version updated to 0.34.0.

--- docs/folderDictionary.js
+++ docs/folderDictionary.js
@@ -3,6 +3,6 @@
 //
 var folderDictionary = {
     'footer-macro': 'footer@templates.html',
-    versionNumber: '0.33.0',
-    versionDate: '8 May 2019'
+    versionNumber: '0.34.0',
+    versionDate: '23 May 2019'
 };
