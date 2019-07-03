535418863bdb85599fd2010f89a3c73ece6a1695
David Cana <david.javapagetemplates@gmail.com>
2019-07-03 14:17:18 +0200

    Added some methods to utils to remove jquery's dependency: isArray, isPlainObject, deepExtend, extend and getJSON.

--- js/app/utils.js
+++ js/app/utils.js
@@ -16,7 +16,92 @@ module.exports = (function() {
         return result;
     }
     
+    var isArray = Array.isArray;
+    
+    var isPlainObject = function( obj ) {
+        var proto, Ctor;
+
+        // Detect obvious negatives
+        // Use toString instead of jQuery.type to catch host objects
+        if ( !obj || toString.call( obj ) !== "[object Object]" ) {
+            return false;
+        }
+
+        proto = getProto( obj );
+
+        // Objects with no prototype (e.g., `Object.create( null )`) are plain
+        if ( !proto ) {
+            return true;
+        }
+
+        // Objects with prototype are plain iff they were constructed by a global Object function
+        Ctor = hasOwn.call( proto, "constructor" ) && proto.constructor;
+        return typeof Ctor === "function" && fnToString.call( Ctor ) === ObjectFunctionString;
+    };
+    var getProto = Object.getPrototypeOf;
+    
+    var deepExtend = function( out ) {
+        out = out || {};
+
+        for ( var i = 1; i < arguments.length; i++ ) {
+            var obj = arguments[ i ];
+
+            if ( ! obj )
+                continue;
+
+            for ( var key in obj ) {
+                if ( obj.hasOwnProperty( key ) ) {
+                    if ( typeof obj[ key ] === 'object' )
+                        out[ key ] = deepExtend( out[ key ], obj[ key ] );
+                    else
+                        out[ key ] = obj[ key ];
+                }
+            }
+        }
+
+        return out;
+    };
+    
+    var extend = function(out) {
+        out = out || {};
+
+        for ( var i = 1; i < arguments.length; i++ ) {
+            if ( ! arguments[ i ] )
+                continue;
+
+            for ( var key in arguments[ i ] ) {
+                if ( arguments[ i ].hasOwnProperty( key ) )
+                    out[ key ] = arguments[ i ][ key ];
+            }
+        }
+
+        return out;
+    };
+    
+    var getJSON = function( url, doneListener, failListener ){
+        
+        var oReq = new window.XMLHttpRequest();
+        oReq.addEventListener( 
+            "load",
+            function(){
+                doneListener( 
+                    JSON.parse( 
+                        oReq.responseText 
+                    ) 
+                );
+            }
+        );
+        oReq.addEventListener( "error", failListener );
+        oReq.open( "GET", url );
+        oReq.send();
+    };
+    
     return {
-        generateId: generateId
+        generateId: generateId,
+        isArray: isArray,
+        isPlainObject: isPlainObject,
+        deepExtend: deepExtend,
+        extend: extend,
+        getJSON: getJSON
     };
 })();
