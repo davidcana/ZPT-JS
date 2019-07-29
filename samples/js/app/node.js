d30d7f5a8fc11312250914316d03f1a367c4242b
David Cana <david.javapagetemplates@gmail.com>
2019-07-29 09:45:52 +0200

    node-jsdom dependency replaced by jsdom due it is no longer maintained. New file added: package-lock.json.

--- samples/js/app/node.js
+++ samples/js/app/node.js
@@ -1,50 +1,36 @@
 "use strict";
 
-// node-jsdom: it is needed when no browser is available
-var jsdom = require( 'node-jsdom' ).jsdom;
+// jsdom: it is needed when no browser is available
+var jsdom = require( 'jsdom' );
+var { JSDOM } = jsdom;
 
-jsdom.env(
-    '<!doctype html>'
+// Build JSDOM instance
+var dom = new JSDOM(
+    '<!DOCTYPE html>'
 	+ '<html>'
 	+ '<body><h1 id="t1" data-content="string:hello">a text</h1></body>'
-	+ '</html>', 
-    /*[ '../../../js/lib/jquery-2.0.3.js' ], */
-    [ 'http://code.jquery.com/jquery.min.js' ], 
-    function( err, window ) {
-        
-        // Check if an error occurs
-        if ( err ) {
-            console.error( err );
-            return 1;
-        }
-
-        // Copy window to global
-        global.window = window;
-        
-        // Copy from window to local vars
-        var $ = window.$;
-        var document = window.document;
-        //console.log( document.body.innerHTML );
-
-        // Parse template
-        var zpt = require( '../../../js/app/main.js' );
-        
-        zpt.run({
-            root: document.body,
-            dictionary: {},
-            callback: function(){
-                console.log( 'Done!' );
-                //console.log( $('#t1').html() );
-            }
-        });
-        
-        // Some tests
-        if ( $('#t1').html() !== "hello" ){
-            console.log( 'ZPT NOT working properly! ' + $('#t1').html() );
-            return 1;
-        }
-
-        console.log( 'ZPT working properly!' );
-    }
+	+ '</html>'
 );
 
+// Init some important vars
+var window = dom.window;
+var document = window.document;
+global.window = window;
+
+// Parse template
+var zpt = require( '../../../js/app/main.js' );
+zpt.run({
+    root: document.body,
+    dictionary: {}
+});
+
+// Do some tests
+var actual = document.getElementById( 't1' ).innerHTML;
+var expected = 'hello';
+if ( actual !== expected ){
+    console.log( 'ZPT NOT working properly using node.js! \nt1.innerHtml = ' + actual );
+    return 1;
+}
+
+console.log( 'ZPT working properly using node.js!' );
+return 0;
