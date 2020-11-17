476fb182f946cf082b021974681c0e12f921ebe3
David Cana <david.javapagetemplates@gmail.com>
2020-11-17 11:06:09 +0100

    ZPT-Js version updated in docs/folderDictionary.js.

--- docs/folderDictionary.js
+++ docs/folderDictionary.js
@@ -3,6 +3,6 @@
 //
 var folderDictionary = {
     'footer-macro': 'footer@templates.html',
-    versionNumber: '0.40.3',
-    versionDate: '13 Sep 2020'
+    versionNumber: '0.40.4',
+    versionDate: '17 Nov 2020'
 };
