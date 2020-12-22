97e0828eed6ceb3b3c13562fd20621a4db218f61
David Cana <david.javapagetemplates@gmail.com>
2020-12-22 09:06:23 +0100

    Version updated to 0.40.6 in docs/folderDictionary.js and in js/app/version.js.

--- docs/folderDictionary.js
+++ docs/folderDictionary.js
@@ -3,6 +3,6 @@
 //
 var folderDictionary = {
     'footer-macro': 'footer@templates.html',
-    versionNumber: '0.40.4',
-    versionDate: '17 Nov 2020'
+    versionNumber: '0.40.6',
+    versionDate: '22 Dic 2020'
 };
