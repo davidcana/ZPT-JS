85c803e9c669f6c92f801a3bc8f866d8dde670d8
David Cana <david.javapagetemplates@gmail.com>
2025-09-08 12:46:26 +0200

    Web in docs directory updated to use ZPT as a es module. Contents not updated yet.

--- docs/js/updatingExample.js
+++ docs/js/updatingExample.js
@@ -1,3 +1,5 @@
+import { zpt } from '../lib/zpt-esm.js';
+
 // This is needed to make the git pages work
 zpt.context.getConf().externalMacroPrefixURL = location.pathname.startsWith( '/ZPT-JS' )? '/ZPT-JS/': '/';
 
